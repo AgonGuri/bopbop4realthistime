8b9fe0d9f6c0d7b2ff05a0988e9f575aaecc647e
eyeofthedragon <eileencarette@gmail.com>
2025-04-12 11:32:02 +0200

    managing cutscenes whee

--- Scripts/CutSceneManager.gd
+++ Scripts/CutSceneManager.gd
@@ -1,11 +1,37 @@
 extends Node2D
 
+var introDialogueCalled
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	DialogueManager.get_current_scene = func():
+		return get_node(".")
+	
+	#DialogueManager.show_example_dialogue_balloon(load("res://dialogue/dialogue.dialogue"), "introCutscene")
+	introDialogueCalled = false
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if (!introDialogueCalled):
+		introDialogueCalled = true
+		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/dialogue.dialogue"), "introCutscene")
+		
+		
+	#okay so this will depend on other stuff
+	#like
+	#if gameOver:
+		#DialogueManager.show_example_dialogue_balloon(load("res://dialogue/dialogue.dialogue"), "youLose")
+		
+		
+	#if gameWon:
+		#DialogueManager.show_example_dialogue_balloon(load("res://dialogue/dialogue.dialogue"), "youWin")
+
+
+func StartGame():
+	print("hello")
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+	
+#and then I'll have other functions that are called at the end of youWon, gameOver
+#which will call their respective scenes
+#once those exist or whatever

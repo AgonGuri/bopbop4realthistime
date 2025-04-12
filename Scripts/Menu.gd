10736711f5af60c794433bbf77476f46db55a09f
Gandaris <pasa.mieslinger@gmail.com>
2025-04-12 10:40:30 +0200

    Basic Main Menu Structure
    Added Main Menu but uuuuugly

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -7,5 +7,15 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/CutSceneManager.tscn")
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()

7a204e0b7f3bc341cbc520d335d22a28b615d5c4
cipher wave <wavemfw@gmail.com>
2025-04-12 21:07:01 +0200

    animation

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+#Spites animation on beat
+@onready var villagers_foreground = $MainUi/VillagersForeground2
+@onready var villagers_background = $MainUi/VillagersBackground
+
+#Animation parameters
+var pulse_scale_x = 1
+var pulse_scale_y = 1
+var base_scale: Vector2
+
 var score = 0
 var combo = 0
 
@@ -33,6 +42,7 @@ var SECOND_LANE_SPAWN = Vector2(POS_X, LANE_WIDTH * 2)
 var THIRD_LANE_SPAWN = Vector2(POS_X, LANE_WIDTH * 3)
 var FOURTH_LANE_SPAWN = Vector2(POS_X, LANE_WIDTH * 4)
 
+
 func _ready():
 	randomize()
 	$Conductor.play_with_beat_offset(8)
@@ -40,6 +50,9 @@ func _ready():
 	$ArrowOverlays/ArrowRIGHT.position = SECOND_LANE_SPAWN
 	$ArrowOverlays/ArrowLEFT.position = THIRD_LANE_SPAWN
 	$ArrowOverlays/ArrowDOWN.position = FOURTH_LANE_SPAWN
+	
+	#for the animation
+	base_scale = villagers_foreground.scale
 	
 	
 	
@@ -126,6 +139,25 @@ func _on_conductor_beat_signal(pos):
 		#Global.missed = missed
 		if get_tree().change_scene_to_file("res://Scenes/End.tscn") != OK:
 			print ("Error changing scene to End")
+			
+	#Animation of the sprites
+	villagers_foreground.scale.x = pulse_scale_x
+	villagers_foreground.scale.y = pulse_scale_y
+	
+	var pulse_scale = Vector2(pulse_scale_x, pulse_scale_y)
+
+	# Tween: squash and stretch, then go back
+	var tween1 = get_tree().create_tween()
+	tween1.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
+	tween1.tween_property(villagers_foreground, "scale", pulse_scale, 0.02)
+	tween1.tween_property(villagers_foreground, "scale", base_scale, 0.1).set_delay(0.05)
+	
+	var tween2 = get_tree().create_tween()
+	tween2.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
+	tween2.tween_property(villagers_background, "scale", pulse_scale, 0.02)
+	tween2.tween_property(villagers_background, "scale", base_scale, 0.1).set_delay(0.05)
 
 
 

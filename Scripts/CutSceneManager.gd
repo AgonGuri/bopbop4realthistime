00e98d19ee16d469db124e91779b0a5433604336
eyeofthedragon <eileencarette@gmail.com>
2025-04-12 11:58:48 +0200

    main menu basics

--- Scripts/CutSceneManager.gd
+++ Scripts/CutSceneManager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var introDialogueCalled
+var startedFromMenu = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,7 +14,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (!introDialogueCalled):
+	if (!introDialogueCalled and startedFromMenu):
 		introDialogueCalled = true
 		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/dialogue.dialogue"), "introCutscene")
 		
--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -3,7 +3,18 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var startButton = $StartButton
+	startButton.pressed.connect(self._startButton_pressed)
+	
+	var quitButton = $QuitButton
+	quitButton.pressed.connect(self._quitButton_pressed)
+
+func _startButton_pressed():
+	CutSceneManager.startedFromMenu = true
+	get_tree().change_scene_to_file("res://Scenes/IntroCutscene.tscn")
+
+func _quitButton_pressed():
+	get_tree().quit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
